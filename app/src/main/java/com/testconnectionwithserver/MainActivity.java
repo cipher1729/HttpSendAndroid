7e42ff162448405d1cd1aec5366e61c851a470f9
Chandan Jyoti Sharma <chandan.jyoti.sharma@intel.com>
2015-10-08 17:24:02 -0700

    sending JSON object

--- app/src/main/java/com/testconnectionwithserver/MainActivity.java
+++ app/src/main/java/com/testconnectionwithserver/MainActivity.java
@@ -5,9 +5,12 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -25,8 +28,9 @@ import java.util.List;
 
 
 public class MainActivity extends Activity {
-    String url="http://mapheroku.herokuapp.com/test";
+    String url="http://10.170.196.118:3000/addPath";
     float[] sampleValues ={-90.03f, 20.33f};
+    InputStream is;
     //String url="http://192.168.0.17:3000";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +41,7 @@ public class MainActivity extends Activity {
 
         Thread t = new Thread(runnable);
         t.start();
+        Toast.makeText(MainActivity.this, "Posted", Toast.LENGTH_LONG).show();
 
     }
 
@@ -50,28 +55,49 @@ public class MainActivity extends Activity {
                // httpURLConnection.connect();
                 httpURLConnection.setDoOutput(true);
                 httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setRequestProperty("Content-Type", "application/json");
                 OutputStream os = httpURLConnection.getOutputStream();
                 BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                JSONObject recordObject= new JSONObject();
+                recordObject.put("origin", "delhi");
+                recordObject.put("destination", "denver");
+                JSONArray boundsArray = new JSONArray();
+                JSONObject firstBoundObject = new JSONObject();
+                JSONObject secondBoundObject = new JSONObject();
 
-                Uri.Builder builder = new Uri.Builder();
+                /* creating first object and inserting at first position of array*/
+                firstBoundObject.put("latNE", 23.00);
+                firstBoundObject.put("longNE", 12.00);
+                firstBoundObject.put("latSW", -23.00);
+                firstBoundObject.put("longSW", -23.00);
+                secondBoundObject.put("latNE", 11.00);
+                secondBoundObject.put("longNE", 11.00);
+                secondBoundObject.put("latSW", -113.00);
+                secondBoundObject.put("longSW", -11.00);
+
+                boundsArray.put(0,firstBoundObject);
+                boundsArray.put(1, secondBoundObject);
+                recordObject.put("bounds", boundsArray);
+                /*Uri.Builder builder = new Uri.Builder();
                 String latitude="lat";
+
                 for(int i=0;i<sampleValues.length;i++)
                 {
                     builder.appendQueryParameter(latitude+Integer.toString(i), Float.toString(sampleValues[i]));
                 }
 
-                /*Uri.Builder builder = new Uri.Builder().appendQueryParameter("something","elza");
-                        //.appendQueryParameter("second","two")
-                        //.appendQueryParameter("third","three");
-                */
 
                 String query = builder.build().getEncodedQuery();
 
                 bufferedWriter.write(query);
+                */
+
+                bufferedWriter.write(recordObject.toString());
                 bufferedWriter.flush();
                 bufferedWriter.close();
                 httpURLConnection.connect();
-                InputStream is = httpURLConnection.getInputStream();
+
+                is = httpURLConnection.getInputStream();
                 BufferedReader bufferedReader= new BufferedReader(new InputStreamReader(is,"UTF-8"));
                 String line;
                 StringBuilder sb = new StringBuilder();
@@ -86,6 +112,7 @@ public class MainActivity extends Activity {
                  os.close();
 
             } catch (Exception e) {
+
                 e.printStackTrace();
             }
 
